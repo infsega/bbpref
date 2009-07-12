33dd6f65577866648b7ff51f62bf45a5b80520cc
Konstantin Tokarev <annulen@users.sourceforge.net>
2009-07-12 17:25:54 +0400

    Many changes: added i18n support and Russian translation, changed UI icons, fixed bug in bidding (user could stay whenever he whished and computer couldn't; now only person who started bidding can do it), options dialogs changed

---
+++ openpref_ru.ts
@@ -0,0 +1,300 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>FormBid</name>
+    <message>
+        <source>Show game table with calculated scores</source>
+        <translation>Отображает пулю с рассчитанным счетом</translation>
+    </message>
+    <message>
+        <source>&amp;Pass</source>
+        <translation>&amp;Пас</translation>
+    </message>
+    <message>
+        <source>&amp;Whist</source>
+        <translation>&amp;Вист</translation>
+    </message>
+    <message>
+        <source>&amp;Misere</source>
+        <translation>&amp;Мизер</translation>
+    </message>
+    <message>
+        <source>S&amp;core</source>
+        <translation>С&amp;чет</translation>
+    </message>
+    <message>
+        <source>Get &amp;back</source>
+        <translation type="unfinished">&amp;Назад</translation>
+    </message>
+    <message>
+        <source>&amp;HalfWhist</source>
+        <translation type="unfinished">По&amp;лвиста</translation>
+    </message>
+    <message>
+        <source>&amp;Halfwhist</source>
+        <translation type="obsolete">Пол&amp;виста</translation>
+    </message>
+</context>
+<context>
+    <name>Kpref</name>
+    <message>
+        <source>&amp;Game</source>
+        <translation>&amp;Игра</translation>
+    </message>
+    <message>
+        <source>&amp;New game...</source>
+        <translation>&amp;Новая игра...</translation>
+    </message>
+    <message>
+        <source>&amp;Open...</source>
+        <translation>&amp;Открыть...</translation>
+    </message>
+    <message>
+        <source>&amp;Save</source>
+        <translation type="unfinished">&amp;Сохранить</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>&amp;Параметры...</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>Вы&amp;ход</translation>
+    </message>
+    <message>
+        <source>&amp;Show</source>
+        <translation>По&amp;казать</translation>
+    </message>
+    <message>
+        <source>S&amp;core...</source>
+        <translation>С&amp;чет...</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Справка</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>&amp;About...</source>
+        <translation>&amp;О программе...</translation>
+    </message>
+    <message>
+        <source>OpenPref
+developer version (0.1.2)
+http://gitorious.org/openprefqt4
+
+(c) 2004 Fedotov A.V.
+Based on kpref (c) Azarniy I.V.
+Qt4 port and many new bugs by Ketmar
+Some bells and whistles by Annulen</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NewGameDialog</name>
+    <message>
+        <source>New Game</source>
+        <translation>Новая игра</translation>
+    </message>
+    <message>
+        <source>Players</source>
+        <translation>Игроки</translation>
+    </message>
+    <message>
+        <source>use &amp;AlphaBeta</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Conventions</source>
+        <translation>Конвенции</translation>
+    </message>
+    <message>
+        <source>Whist type:</source>
+        <translation>Тип виста:</translation>
+    </message>
+    <message>
+        <source>&amp;Stalingrad</source>
+        <translation>&amp;Сталинград</translation>
+    </message>
+    <message>
+        <source>10 game</source>
+        <translation>Десятерная игра</translation>
+    </message>
+    <message>
+        <source>Check</source>
+        <translation>Проверяется</translation>
+    </message>
+    <message>
+        <source>Whist</source>
+        <translation>Вистуется</translation>
+    </message>
+    <message>
+        <source>Pool size:</source>
+        <translation>Размер пули:</translation>
+    </message>
+    <message>
+        <source>&amp;greedy</source>
+        <translation>&amp;жлобский</translation>
+    </message>
+    <message>
+        <source>Pass-out:</source>
+        <translation>Распасы:</translation>
+    </message>
+    <message>
+        <source>6-7-7...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>6 of spades:</source>
+        <translation>6 пик:</translation>
+    </message>
+</context>
+<context>
+    <name>OptDialog</name>
+    <message>
+        <source>Options</source>
+        <translation>Параметры</translation>
+    </message>
+    <message>
+        <source>Pool size:</source>
+        <translation type="obsolete">Размер пули:</translation>
+    </message>
+    <message>
+        <source>Whist type:</source>
+        <translation type="obsolete">Тип виста:</translation>
+    </message>
+    <message>
+        <source>&amp;greedy</source>
+        <translation type="obsolete">жлобский</translation>
+    </message>
+    <message>
+        <source>10 game:</source>
+        <translation type="obsolete">Десятерная игра:</translation>
+    </message>
+    <message>
+        <source>&amp;check</source>
+        <translation type="obsolete">&amp;проверять</translation>
+    </message>
+    <message>
+        <source>6 of spades:</source>
+        <translation type="obsolete">6 пик</translation>
+    </message>
+    <message>
+        <source>&amp;Stalingrad</source>
+        <translation type="obsolete">&amp;Сталинград</translation>
+    </message>
+    <message>
+        <source>Deal:</source>
+        <translation type="obsolete">Сдача:</translation>
+    </message>
+    <message>
+        <source>Take:</source>
+        <translation type="obsolete">Взятка:</translation>
+    </message>
+    <message>
+        <source>&amp;animated</source>
+        <translation type="obsolete">с &amp;анимацией</translation>
+    </message>
+    <message>
+        <source>a&amp;nimated</source>
+        <translation type="obsolete">с а&amp;нимацией</translation>
+    </message>
+    <message>
+        <source>Alphabeta:</source>
+        <translation type="obsolete">Альфабета:</translation>
+    </message>
+    <message>
+        <source>AI cards:</source>
+        <translation type="obsolete">Карты компьютера:</translation>
+    </message>
+    <message>
+        <source>Sho&amp;w (debug)</source>
+        <translation type="obsolete">По&amp;казать</translation>
+    </message>
+    <message>
+        <source>Pass-out:</source>
+        <translation type="obsolete">Распасы</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="obsolete">Отмена</translation>
+    </message>
+    <message>
+        <source>O&amp;K</source>
+        <translation type="obsolete">O&amp;K</translation>
+    </message>
+    <message>
+        <source>Animation</source>
+        <translation>Анимация</translation>
+    </message>
+    <message>
+        <source>Animate &amp;deal</source>
+        <translation>Анимировать &amp;сдачу</translation>
+    </message>
+    <message>
+        <source>Animate &amp;takes</source>
+        <translation>Анимировать &amp;взятки</translation>
+    </message>
+    <message>
+        <source>Sho&amp;w AI cards (debug)</source>
+        <translation>&amp;Показать карты компьютера (отладка)</translation>
+    </message>
+</context>
+<context>
+    <name>PrefDesktop</name>
+    <message>
+        <source>thinking...</source>
+        <translation>думаю...</translation>
+    </message>
+    <message>
+        <source>PASS</source>
+        <translation type="obsolete">ПАС</translation>
+    </message>
+    <message>
+        <source>WHIST</source>
+        <translation type="obsolete">ВИСТ</translation>
+    </message>
+    <message>
+        <source>whist</source>
+        <translation>вист</translation>
+    </message>
+    <message>
+        <source>pass</source>
+        <translation>пас</translation>
+    </message>
+    <message>
+        <source>6 no trump</source>
+        <translation>6 без козыря</translation>
+    </message>
+    <message>
+        <source>7 no trump</source>
+        <translation>7 без козыря</translation>
+    </message>
+    <message>
+        <source>8 no trump</source>
+        <translation>8 без козыря</translation>
+    </message>
+    <message>
+        <source>9 no trump</source>
+        <translation>9 без козыря</translation>
+    </message>
+    <message>
+        <source>10 no trump</source>
+        <translation>10 без козыря</translation>
+    </message>
+    <message>
+        <source>Select cards to drop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Try to remember the cards</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Misere</source>
+        <translation>Мизер</translation>
+    </message>
+</context>
+</TS>
---
+++ src/newgameform.cpp
@@ -0,0 +1,29 @@
+
+#include <QDesktopWidget>
+#include <QDialog>
+
+#include "newgameform.h"
+
+NewGameDialog::NewGameDialog (QWidget *parent) : QDialog(parent) {
+  setupUi(this);
+  setAttribute(Qt::WA_QuitOnClose, false);
+  setAttribute(Qt::WA_DeleteOnClose, false);
+}
+
+
+void NewGameDialog::showEvent (QShowEvent *event) {
+  Q_UNUSED(event)
+  QWidget *parentObj = dynamic_cast<QWidget *>(parent());
+  int x, y;
+  if (parentObj) {
+    QRect dims(parentObj->frameGeometry());
+    x = dims.left()+(dims.width()-width())/2;
+    y = dims.top()+(dims.height()-height())/2;
+  } else {
+    QDesktopWidget *desk = QApplication::desktop();
+    QRect dims(desk->availableGeometry(this));
+    x = dims.left()+(dims.width()-width())/2;
+    y = dims.top()+(dims.height()-height())/2;
+  }
+  move(x, y);
+}
