1eca1f5f99307d8e847fabe4ebdf9a0e3cd47a74
Konstantin Tokarev <annulen@users.sourceforge.net>
2009-09-25 01:17:27 +0400

    Moved OpenPref to GPLv3

--- src/forms/optform.h
+++ src/forms/optform.h
@@ -1,13 +1,25 @@
-/* OpenPref - cross-platform Preferans game
- * 
- * coded by Ketmar // Avalon Group (psyc://ketmar.no-ip.org/~Ketmar)
- *
- * This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details.
+/*
+ *      OpenPref - cross-platform Preferans game
+ *      
+ *      Copyright (C) 2000-2009 OpenPref Developers
+ *      (see file AUTHORS for more details)
+ *      Contact: annulen@users.sourceforge.net
+ *      
+ *      OpenPref is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 3 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program (see file COPYING); if not, see 
+ *      http://www.gnu.org/licenses 
  */
+
 #ifndef OPTFORM_H
 #define OPTFORM_H
 
