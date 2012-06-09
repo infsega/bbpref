3f9df58de18cc7dfdc9fa051faa2e146c9fb93ce
Sergey Gagarin <infsega@gmail.com>
2012-06-10 02:40:26 +0400

    Game history properly translated

--- src/view/gamelogdialog.cpp
+++ src/view/gamelogdialog.cpp
@@ -1,111 +1,11 @@
 #include "gamelogdialog.h"
 #include "ui_gamelogdialog.h"
 
-#include <QAbstractTableModel>
-#include <QList>
 #include <QMessageBox>
+#include <QStandardItemModel>
 
 #include "desktop.h"
 #include "player.h"
-
-class GameLogModel : public QAbstractTableModel
-{
-private:
-  QList<GameLogEntry> m_log;
-  PrefModel *m_model;
-
-public:
-  explicit GameLogModel(PrefModel *model, QObject * parent = 0)
-  : QAbstractTableModel(parent)
-  , m_model(model)
-  {
-    setObjectName("GameLogModel");
-    m_log = model->gameLog();
-  }
-
-  int rowCount(const QModelIndex &parent = QModelIndex()) const
-  {
-    Q_UNUSED(parent)
-    return m_log.size();
-  }
-
-  int columnCount(const QModelIndex &parent = QModelIndex()) const
-  {
-    Q_UNUSED(parent)
-    return 6;
-  }
-
-  QVariant data(const QModelIndex &index, int role) const
-  {
-    if(!index.isValid())
-      return QVariant();
-    if (role == Qt::TextAlignmentRole)
-      return Qt::AlignCenter;
-    if (role == Qt::DisplayRole)
-    {
-      switch(index.column())
-      {
-      case 0:
-        {
-          QString s(sGameName(m_log.at(index.row()).game));
-          s.replace("\1s", QChar((ushort)0x2660));
-          s.replace("\1c", QChar((ushort)0x2663));
-          s.replace("\1d", QChar((ushort)0x2666));
-          s.replace("\1h", QChar((ushort)0x2665));
-          s = s.trimmed();
-          return tr(s.toAscii());
-        }
-      case 1:
-      case 2:
-      case 3:
-        return QString::number(m_log.at(index.row()).takes[index.column()-1])
-             + (index.column() == m_log.at(index.row()).player ? "*" : "")
-             + (m_log.at(index.row()).whist[index.column()-1] ? tr("(w)") : "");
-      case 4:
-        return QString::number(m_log.at(index.row()).time, 'f', 1);
-      case 5:
-        return tr("Show");
-      }
-    }
-    return QVariant();
-  }
-
-  Qt::ItemFlags flags(const QModelIndex &index) const
-  {
-    Q_UNUSED(index)
-    return Qt::ItemIsEnabled;
-  }
-
-  QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const
-  {
-    // handle text alignments
-    if (role == Qt::TextAlignmentRole)
-      if (orientation == Qt::Vertical)
-        return Qt::AlignHCenter; // XYZ coordinates
-
-    if (role != Qt::DisplayRole)
-      return QVariant();
-
-    if (orientation == Qt::Horizontal)
-    {
-      switch (section)
-      {
-      case 0:
-        return tr("Game");
-      case 1:
-      case 2:
-      case 3:
-        return m_model->player(section)->nick();
-      case 4:
-        return tr("Time");
-      case 5:
-        return tr("Deal");
-      }
-    }
-
-    return QVariant();
-  }
-};
 
 DealButton::DealButton(int rowNum, const QString & text, QWidget * parent)
 : QPushButton(text, parent)
@@ -120,9 +20,47 @@ GameLogDialog::GameLogDialog(PrefModel* i_model, QWidget *parent)
 , model(i_model)
 {
   ui->setupUi(this);
-  ui->tableView->setModel(new GameLogModel(model, this));
+  QStandardItemModel* treeModel = new QStandardItemModel(this);
+  ui->tableView->setModel(treeModel);
+  QStringList headers;
+  headers.append(tr("Game"));
+  headers.append(model->player(1)->nick());
+  headers.append(model->player(2)->nick());
+  headers.append(model->player(3)->nick());
+  headers.append(tr("Time"));
+  headers.append(tr("Deal"));
+  treeModel->setHorizontalHeaderLabels(headers);
   for (int i = 0; i < model->gameLog().size(); i++)
   {
+    GameLogEntry entry = model->gameLog().at(i);
+    QList<QStandardItem*> items;
+
+    QString gameTitle = sGameName(entry.game);
+    gameTitle.replace("\1s", QChar((ushort)0x2660));
+    gameTitle.replace("\1c", QChar((ushort)0x2663));
+    gameTitle.replace("\1d", QChar((ushort)0x2666));
+    gameTitle.replace("\1h", QChar((ushort)0x2665));
+    items.append( new QStandardItem(gameTitle.trimmed()) );
+
+    QMap<int, QString> playerResult;
+    for ( int player = 1; player <= 3; player++ )
+    {
+      QString& res = playerResult[player];
+      res = QString::number(entry.takes[player - 1]);
+      if (player == entry.player)
+        res += "*";
+      if (entry.whist[player - 1])
+        res += tr("(w)");
+      items.append( new QStandardItem(res) );
+    }
+
+    QString time = QString::number(entry.time, 'f', 1);
+    items.append( new QStandardItem(time) );
+    QString dummy = tr("Show");
+    items.append( new QStandardItem(dummy) );
+
+    treeModel->appendRow(items);
+
     DealButton *btnDeal = new DealButton(i, tr("Deal #") + QString::number(i + 1), ui->tableView);
     ui->tableView->setIndexWidget(ui->tableView->model()->index(i, 5), btnDeal);
     connect(btnDeal, SIGNAL(clicked(int)), this, SLOT(showDeal(int)));
