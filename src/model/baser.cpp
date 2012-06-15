ef98885f91f03279473fe04a54d08384ab5dd2b4
Sergey Gagarin <infsega@gmail.com>
2012-06-16 02:22:09 +0400

    Less portable, but nice serialization of integers

--- src/model/baser.cpp
+++ src/model/baser.cpp
@@ -27,32 +27,20 @@
 
 void serializeInt (QByteArray &ba, int i)
 {
-  for (int f = 3; f >= 0; f--)
-  {
-    unsigned char c = (i&0xff); i >>= 8;
-    ba.append((char)c);
-  }
+  qDebug() << "serialize " << i;
+  ba.append( reinterpret_cast<const char*>(&i), sizeof(int) );
 }
 
 bool unserializeInt (QByteArray &ba, int *pos, int *i)
 {
-  unsigned char buf[4];
-  if (*pos+4 > ba.size())
+  if (*pos + sizeof(int) > ba.size())
   {
     qDebug() << "Not enough space for int";
     return false;
   }
-  const unsigned char *u = (const unsigned char *)ba.constData();
-  u += *pos;
-  *pos += 4;
-  for (int f = 3; f >= 0; f--)
-    buf[f] = *u++;
-  *i = 0;
-  for (int f = 0; f < 4; f++)
-  {
-    *i <<= 8;
-    *i |= buf[f];
-  }
+  *i = *(reinterpret_cast<const int*>(ba.constData() + *pos));
+  qDebug() << "deserialize " << *i;
+  *pos += sizeof(int);
   return true;
 }
 
